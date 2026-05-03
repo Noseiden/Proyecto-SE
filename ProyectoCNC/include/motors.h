6f0a9bbec6a6dce3fe526d6f41c9e3ad6eb24c4f
Kevin <vargaskev2005@gmail.com>
2026-05-02 21:25:32 -0500

    Arquitectura: programación modular

---
+++ ProyectoCNC/include/motors.h
@@ -0,0 +1,7 @@
+#ifndef MOTOR //Header guards
+#define MOTOR
+#pragma once //para evitar que un archivo header sea incluido varias veces
+
+
+
+#endif
---
+++ ProyectoCNC/include/rtc_ds1307.h
@@ -0,0 +1,7 @@
+#ifndef RTC //Header guards
+#define RTC
+#pragma once //para evitar que un archivo header sea incluido varias veces
+
+
+
+#endif
