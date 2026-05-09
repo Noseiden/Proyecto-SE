bad7d123c3de52f83e66226d3a474a9f124bab6f
Kevin <vargaskev2005@gmail.com>
2026-05-08 19:02:40 -0500

    Cambio en pines ADCs para mejor implementación en PCB

--- Firmware_ESP32/ProyectoCNC/src/current_sensor.c
+++ Firmware_ESP32/ProyectoCNC/src/current_sensor.c
@@ -21,16 +21,16 @@ void I_sensor_init(void){
         .atten = ADC_ATTEN_DB_12, //voltaje de referencia: atenuación de 12dB, es decir, 0 a 3.3V
     };
     
-    //Canal 0: GPIO36, sensor de corriente para ruteadora
+    //Canal 0: GPIO36, sensor de corriente para motor X
     adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_0,
     &chan_config); 
-    //Canal 3: GPIO39, sensor de corriente para motor X
+    //Canal 3: GPIO39, sensor de corriente para motor Y
     adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_3,
     &chan_config); 
-    //Canal 6: GPIO34, sensor de corriente para motor Y
+    //Canal 6: GPIO34, sensor de corriente para motor Z
     adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_6,
     &chan_config); 
-    //Canal 7: GPIO35, sensor de corriente para motor Z
+    //Canal 7: GPIO35, sensor de corriente para ruteadora
     adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_7,
     &chan_config);
     
@@ -48,13 +48,13 @@ bool read_I_sensor(consumo_cnc_t *data){
     if(timer_value >= SAMPLE_PERIOD_US){
         int adc_raw;
         //Ingresar datos a la estructura data (de puntero *data a estructura):
-        adc_oneshot_read(adc1_handle, ADC_CHANNEL_0, &adc_raw); 
+        adc_oneshot_read(adc1_handle, ADC_CHANNEL_7, &adc_raw); 
         data->s_I = calc_I(adc_raw); // -> significa ve a (*data) y accede a s_I
-        adc_oneshot_read(adc1_handle, ADC_CHANNEL_3, &adc_raw); 
+        adc_oneshot_read(adc1_handle, ADC_CHANNEL_0, &adc_raw); 
         data->x_I = calc_I(adc_raw); //O también decir (*data).x_I
-        adc_oneshot_read(adc1_handle, ADC_CHANNEL_6, &adc_raw);
+        adc_oneshot_read(adc1_handle, ADC_CHANNEL_3, &adc_raw);
         data->y_I = calc_I(adc_raw);
-        adc_oneshot_read(adc1_handle, ADC_CHANNEL_7, &adc_raw);
+        adc_oneshot_read(adc1_handle, ADC_CHANNEL_6, &adc_raw);
         data->z_I = calc_I(adc_raw);
 
         timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0);
